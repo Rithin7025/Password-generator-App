e533e87b3aec32b824a6e205df289586264f7dc8
Rithin <rithinrj2001@gmail.com>
2024-02-08 09:39:18 +0530

    created folder structure

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-slate-600 text-lg underline'>Hey , we gonna figure it out</h1>
+    </div>
+  )
+}
+
+export default App 
